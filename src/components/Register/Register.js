276215d5378fa64c384f8cecc0fc07f9893e82b1
Michael Kelley <mkelley010@gmail.com>
2019-01-28 11:34:42 -0800

    endpoint fix for heroku

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -22,7 +22,7 @@ class Register extends Component {
     };
 
     onSubmitRegister = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://boiling-falls-67868.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
